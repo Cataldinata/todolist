626f7103874d2c3798e62319195aad26e5602741
Natã Cataldi <82055143+Cataldinata@users.noreply.github.com>
2023-10-12 15:09:21 -0300

    Commit da aula 1 até a 3

--- src/main/java/br/com/cataldinata/todolist/user/UserController.java
+++ src/main/java/br/com/cataldinata/todolist/user/UserController.java
@@ -1,5 +1,8 @@
 package br.com.cataldinata.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,24 +17,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    
-    /*
-     * String (texto)
-     * Integer (int) (números inteiros)
-     * Double (double) Números 0.0000
-     * Float (float) Números 0.000
-     * char (A C)
-     * Date (data)
-     * void (sem retorno de m[etodos])
-    */
-    /*
-     * Body
-    */
+
+    @Autowired
+    private IUserRepository userRepository;
+
 
     @PostMapping ("/")
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.password);
-        System.out.println(userModel.name);
-        System.out.println(userModel.username);
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+      var user =  this.userRepository.findByUsername(userModel.getUsername());
+
+      if(user != null){
+        //Mensagem de erro
+        //Status code
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe!");
+      }
+
+
+
+      var userCreated = this.userRepository.save(userModel);
+      return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
